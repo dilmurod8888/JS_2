863f34045c9e439f181f3879945d38a8ba5f7d92
dilmurod8888 <dilmurodsalimjonov8888@gail.com>
2024-05-26 18:46:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var name = prompt("Ismingizni kiriting")
+var age = prompt("Yoshingizni kiriting")
+var mXi1 = prompt("Misolni yeching 7+3= ")
+var mXi2 = prompt("Misolni yeching 9-5= ")
+var mXi3 = prompt("Misolni yeching 8*5= ")
+var mXi4 = prompt("Misolni yeching 8/2= ")
+var mXi5 = prompt("Misolni yeching 29%5= ")
+
+console.log("Sizning Ismingiz "+name+" yoshingiz "+age+" da")
+
+if (mXi1 == 10){
+    console.log("7+3="+(7+3)+" Sizning javobingiz to'g'ri = "+mXi1);
+}else{
+    console.log("7+3= "+(7+3)+" Sizning javobingiz no to'g'ri = "+mXi1)
+}
+if (mXi2==4){
+    console.log("9-5= "+(9-5)+" Sizning javobingiz to'g'ri = "+mXi2);
+}else{
+    console.log("9-5="+(9-5)+" Sizning javobingiz no to'g'ri = "+mXi2)
+}
+if (mXi3==40){
+    console.log("8*5= "+(8*5)+" Sizning javobingiz to'g'ri = "+mXi3);
+}else{
+    console.log("8*5= "+(8*5)+" Sizning javobingiz no to'g'ri = "+mXi3)
+}
+if (mXi4==4){
+    console.log("8/2="+(8/2)+" Sizning javobingiz to'g'ri = "+mXi4);
+}else{
+    console.log("8/2="+(8/2)+" Sizning javobingiz no to'g'ri = "+mXi4)
+}
+if (mXi5==4){
+    console.log("29%5= "+(29%5)+" Sizning javobingiz to'g'ri = "+mXi5);
+}else{
+    console.log("29%5= "+(29%5)+" Sizning javobingiz no to'g'ri = "+mXi5)
+}
+alert("Tugadi")
